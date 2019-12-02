cfa37260090b5c679e4b6c6e769bd3096bda4c06
mic216 <michal.wlodarczyk85@wp.pl>
2019-12-02 15:15:42 +0100

    commas after tests added

--- example_tests_reader/src/main/java/com/amazonaws/lambda/dynamodb/TestsReader.java
+++ example_tests_reader/src/main/java/com/amazonaws/lambda/dynamodb/TestsReader.java
@@ -1,9 +1,14 @@
 package com.amazonaws.lambda.dynamodb;
 
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
@@ -15,6 +20,10 @@ import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.spec.ScanSpec;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 public class TestsReader implements RequestStreamHandler {
 
@@ -30,11 +39,15 @@ public class TestsReader implements RequestStreamHandler {
 		scanSpec = scanSpec.withProjectionExpression("recruiter_id, test_id, max_points, min_points, questions");
 		ItemCollection<ScanOutcome> items = tests.scan(scanSpec);
 		
-        Iterator<Item> iterator = items.iterator();
+		outputStream = new BufferedOutputStream(outputStream);
+		Iterator<Item> iterator = items.iterator();
         while (iterator.hasNext()) {
             Item item = iterator.next();
-            String itemAsString = item.toJSONPretty();
-        	outputStream.write(itemAsString.getBytes());
+            String itemAsString = (iterator.hasNext()) ? item.toJSONPretty()+"," : item.toJSONPretty();
+            outputStream.write(itemAsString.getBytes());
+//            itemAsString = itemAsString.replaceAll("\"", Character.toString('"'));
+//            itemAsString = itemAsString.replaceAll("\n", "");
         }
+        outputStream.flush();
     }
 }
