0a03205104928e52a0237d2acbe9a9207d9e0c8c
mic216 <michal.wlodarczyk85@wp.pl>
2020-01-14 21:02:23 +0100

    test removing and candidate removing

---
+++ aws-java-back-end-api/src/main/java/com/serverless/tests/RemoveCandidateHandler.java
@@ -0,0 +1,32 @@
+package com.serverless.tests;
+
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PrimaryKey;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.DeleteItemSpec;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class RemoveCandidateHandler implements RequestStreamHandler {
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        JsonNode rootNode = new ObjectMapper().readValue(inputStream, JsonNode.class);
+        String recruiterId = rootNode.get("recruiterId").asText();
+        String testId = rootNode.get("testId").asText();
+        String username = rootNode.get("username").asText();
+
+        Table tests = DynamoDbController.getTable("Tests");
+        PrimaryKey primaryKey = new PrimaryKey("recruiterId", recruiterId, "testId", testId);
+        Item test = DynamoDbController.getItemByPrimaryKey(primaryKey, tests);
+
+        String candidates = JsonFormatter.removeCandidateFromTestByUsername(test, username);
+        DynamoDbController.updateCandidates(primaryKey, candidates, tests);
+    }
+}
---
+++ aws-java-back-end-api/src/main/java/com/serverless/tests/UpdateTestHandler.java
@@ -0,0 +1,16 @@
+package com.serverless.tests;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class UpdateTestHandler implements RequestStreamHandler {
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        DynamoDbController.getTable("Tests");
+    }
+}
